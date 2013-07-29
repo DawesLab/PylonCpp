58e996ef20fe69e198a0a3cb8926896239893fba
Andrew M.C. Dawes <dawes@pacificu.edu>
2013-07-29 15:43:36 -0700

    set save_data to run config functions

--- Makefile
+++ Makefile
@@ -6,4 +6,8 @@ configure: configure.o
 configure.o: configure.cpp
 	@g++ -I$(INC) -lpicam -c configure.cpp
 
-#TODO make these all generic:
+save_data: save_data.o
+	@g++ -lpicam save_data.o -o save_data
+
+save_data.o: save_data.cpp
+	@g++ -I$(INC) -lpicam -c save_data.cpp
--- save_data.cpp
+++ save_data.cpp
@@ -9,6 +9,100 @@
 
 #include "stdio.h"
 #include "picam.h"
+#include <iostream>
+
+// - prints any picam enum
+void PrintEnumString( PicamEnumeratedType type, piint value )
+{
+    const pichar* string;
+    Picam_GetEnumerationString( type, value, &string );
+    std::cout << string;
+    Picam_DestroyString( string );
+}
+// - prints error code
+void PrintError( PicamError error )
+{
+    if( error == PicamError_None )
+        std::cout << "Succeeded" << std::endl;
+    else
+    {
+        std::cout << "Failed (";
+        PrintEnumString( PicamEnumeratedType_Error, error );
+        std::cout << ")" << std::endl;
+    }
+}
+// - changes some common camera parameters and applies them to hardware
+void Configure( PicamHandle camera )
+{
+    PicamError error;
+
+    // - set low gain
+    std::cout << "Set low analog gain: ";
+    error =
+        Picam_SetParameterIntegerValue(
+            camera,
+            PicamParameter_AdcAnalogGain,
+            PicamAdcAnalogGain_Low );
+    PrintError( error );
+
+    // - set exposure time (in millseconds)
+    std::cout << "Set 210 ms exposure time: ";
+    error = 
+        Picam_SetParameterFloatingPointValue(
+            camera,
+            PicamParameter_ExposureTime,
+            210.0 );
+    PrintError( error );
+    
+    // - set ADC rate (in megahertz)
+    std::cout << "Set 4 MHz ADC rate: ";
+    error = 
+        Picam_SetParameterFloatingPointValue(
+            camera,
+            PicamParameter_AdcSpeed,
+            4.0 );
+    PrintError( error );
+
+
+    // - show that the modified parameters need to be applied to hardware
+    pibln committed;
+    Picam_AreParametersCommitted( camera, &committed );
+    if( committed )
+        std::cout << "Parameters have not changed" << std::endl;
+    else
+        std::cout << "Parameters have been modified" << std::endl;
+
+    // - apply the changes to hardware
+    std::cout << "Commit to hardware: ";
+    const PicamParameter* failed_parameters;
+    piint failed_parameters_count;
+    error =
+        Picam_CommitParameters(
+            camera,
+            &failed_parameters,
+            &failed_parameters_count );
+    PrintError( error );
+
+    std::cout << "Testing for invalid params\n";
+    // - print any invalid parameters
+    if( failed_parameters_count > 0 )
+    {
+        std::cout << "The following parameters are invalid:" << std::endl;
+        for( piint i = 0; i < failed_parameters_count; ++i )
+        {
+            std::cout << "    ";
+            PrintEnumString(
+                PicamEnumeratedType_Parameter,
+                failed_parameters[i] );
+            std::cout << std::endl;
+        }
+    }
+    std::cout << "Cleaning up resources\n";
+    // - free picam-allocated resources
+    Picam_DestroyParameters( failed_parameters );
+}
+
+
 
 void PrintData( pibyte* buf, piint numframes, piint framelength )
 {
@@ -22,6 +116,7 @@ void PrintData( pibyte* buf, piint numframes, piint framelength )
         printf( "%5d,%5d,%5d\t%d\n", *(midpt-1), *(midpt), *(midpt+1), loop+1 );
     }
 }
+
 int main()
 {
     Picam_InitializeLibrary();
@@ -50,6 +145,9 @@ int main()
     printf( "%s", string );
     printf( " (SN:%s) [%s]\n", id.serial_number, id.sensor_name );
     Picam_DestroyString( string );
+
+    //set up the camera settings:
+    Configure( camera );
 
     Picam_GetParameterIntegerValue( camera, PicamParameter_ReadoutStride, &readoutstride );
     printf( "Waiting for %d frames to be collected\n\n", NUM_FRAMES );
